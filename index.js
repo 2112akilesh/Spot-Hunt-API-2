3e3655e0dfdb27571490e4235a448e4a10f0534d
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-01-24 23:23:21 +0530

    All work completed

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const config = require('./config.json');
 const get_rout = require('./routs/get');
 const post_rout = require('./routs/post');
 
+
+//--------------------------------server.js (Auth0)--------------------------------------
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+
+
+// Authorization middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://spothunt.jp.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: '600d1d37fdf85b003d9f23cb',
+  issuer: `https://spothunt.jp.auth0.com/`,
+  algorithms: ['RS256']
+});
+
+//------------------------------------------------------------------------
+// This route doesn't need authentication
+app.get('/api/public', function(req, res) {
+  res.json({
+    message: 'Hello from a public endpoint! You don\'t need to be authenticated to see this.'
+  });
+});
+//-----------------------------------Auth0-Completed------------------------------------------------
+
 //Assigning app to express
 const app = express();
 
--- routs/get.js
+++ routs/get.js
@@ -9,14 +9,14 @@ router.get('/check', async (req, res) => {
   //Saving the req.Photo_DNA value in "x"
   const x = await Gets.findOne({ Photo_DNA: req.body.Photo_DNA });
 
-  //Checking if Photo_DNA if present in D.B or not
+  //Checking if Photo_DNA is present in D.B or not
   try {
     if (x != null)
       res.json(x);
     else
       res.send("you are Data is safe");
   } catch (err) {
-    res.json({ message: err });
+      res.json({ message: err });
   }
 });
 
